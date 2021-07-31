4bf7aa5bd473edf2bec257958b25dc305ef818c1
evelyn82ny <evelyn82ny@gmail.com>
2021-07-31 09:15:08 +0900

    Apply @SessionAttribute provided by spring

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -7,27 +7,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final SessionManager sessionManager;
-
     @GetMapping("/")
-    public String homeLogin(HttpServletRequest httpServletRequest, Model model) {
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
-        HttpSession session = httpServletRequest.getSession(false);
-        if(session == null){
-            return "home";
-        }
-
-        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
-        if(loginMember == null){
+       if(loginMember == null){
             return "home";
         }
 
